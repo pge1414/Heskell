3160c99e9af7aeccddd576134b19bf792eae3194
pge1414 <101663790+pge1414@users.noreply.github.com>
2023-02-07 18:56:47 +0100

    Paul

--- Heskell/intro.hs
+++ Heskell/intro.hs
@@ -1,93 +1,157 @@
 mal2 :: Int -> Int
-mal2 x = 2 * x
+mal2 x = x * 2
+
+fakultät :: Int -> Int
+fakultät 0 = 1
+fakultät n = (fakultät (n - 1)) * n
 
 fibonacci :: Int -> Int
 fibonacci 0 = 0
 fibonacci 1 = 1
-fibonacci x = fibonacci(x-1)+ fibonacci(x-2)
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 
 größeres :: Int -> Int -> Int
 größeres a b
     | a < b = b
     | otherwise = a
 
-erstes :: [Int] -> Int
-erstes [] = -1
+erstes :: [a] -> a
+erstes [] = error ("Liste leer!")
 erstes (x:xs) = x
 
-letztes :: [Int] -> Int
-letztes [] = -1
+letztes :: [a] -> a
+letztes [] = error ("Liste leer!")
 letztes [x] = x
 letztes (x:xs) = letztes xs
 
 summe :: [Int] -> Int
 summe [] = -1
+summe [x] = x
 summe (x:xs) = x + summe xs
 
-nimm :: Int -> [Int] -> Int
-nimm _ [] = -1
-nimm 0 (x:xs) = x
-nimm n (x:xs) nimm (n- 1) xs
+länge :: [a] -> Int
+länge [] = 0
+länge [x] = 1
+länge (x:xs) = länge xs + 1
+
+länges :: [Int] -> Int
+länges [] = 0
+länges (x:xs) = (ceiling (fromIntegral (länge (x:xs)) / 2))
+
+nimm :: Int -> [a] -> a
+nimm n [] = error ("Liste leer!")
+nimm 1 (x:xs) = x
+nimm n (x:xs) 
+    | n > (länge (x:xs)) = error ("Liste leer!")
+    | otherwise = nimm (n - 1) xs
 
 maxi :: [Int] -> Int
-maxi [] = error "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+maxi [] = error ("Liste leer!")
 maxi [x] = x
 maxi (x:y:ys)
-    | x < y = maxi (y:ys)
-    | otherwise = größtes (x:ys)
+    | x > y = maxi (x:ys)
+    | otherwise = maxi (y:ys)
 
 enthalten :: [Int] -> Int -> Bool
 enthalten [] n = False
-enthalten (x:xs) n 
+enthalten (x:xs) n
     | n == x = True
-    | enthalten xs n
-
-sortiert :: [Int] -> Bool
-sortiert [] = False
-sortiert [x] = True
-sortiert (x:y:xs)
-    | x<y<sortiert[xs] = True
-    |otherwise = False
-
-
-index:: [Int] -> Int -> Int
-index [] _ = error "leer"
-index [x] x = 0
-index (x:xs) xs = index (xs-1)
-
-anzahl :: [Int] -> Int -> Int
-anzahl [] _ = 0
-anzahl (x:xs) n
-    |x == n = 1+ anzahl xs n
-    |otherwise = anzahl xs n
-    
-
---länge :: [Int] -> Int
---länge [] = 0
---länge (x:xs) = 1 + länge xs
-
-
---index :: [Int] -> Int -> Int
---index [] n = "leere liste"
---index [x] = 1
---index (x:y:xs) n
- --   | n == 1 = xs
-   -- | n == 2 = y
-    -- | otherwise = index xs
-
---löschen :: [Int] -> Int -> [Int]
---löschen [] n = error "leer"
---löschen (x:xs) n
-   -- | n == x = xs
-   -- | otherwise = x:löschen xs n
-
---umdrehen :: [Int] -> [Int]
---umdrehen [] = []
---umdrehen (x:xs) = umdrehen xs ++ [x]
+    | otherwise = enthalten xs n
 
 entfernen :: [Int] -> Int -> [Int]
-entfernen [] _ = error "leer" 
-entfernen (x:xs) 0 = xs
-entfernen (x:xs) n = x: entfernen xs (n-1)
+entfernen [] _ = error ("Liste leer!")
+entfernen (x:xs) n
+    | n == x = xs
+    | otherwise = entfernen xs (n - 1)
 
+e :: [a] -> [a]
+e [] = error ("Liste leer!")
+e (x:xs) = xs
 
+umdrehen :: [a] -> [a]
+umdrehen [] = []
+umdrehen (x:xs) = umdrehen xs ++ [x]
+
+palindrom :: String -> Bool
+palindrom [] = True
+palindrom [x] = True
+palindrom (x:xs) 
+    | x == nimm (länge (x:xs)) (x:xs) = palindrom (e (umdrehen xs))
+    | otherwise = False
+
+kleinstes :: [Int] -> Int
+kleinstes [] = error ("Liste kleinstes!")
+kleinstes [x] = x
+kleinstes (x:y:ys)
+    | x < y = kleinstes (x:ys)
+    | otherwise = kleinstes (y:ys)
+
+gerade :: Int -> Bool
+gerade x
+    | mod x 2 == 0 = True
+    | otherwise = False
+
+prim_helper :: Int -> Int -> Bool
+prim_helper x y
+    | y > (x - 2) = True
+    | mod x y == 0 = False
+    | otherwise = prim_helper x (y + 1)
+
+prim :: Int -> Bool
+prim 2 = True
+prim x 
+    | x /= 1 && prim_helper x 2 == True = True
+    | otherwise = False
+
+-- sortieren :: [Int] -> [Int]    
+-- sortieren [] = []
+-- sortieren xs = k:sortieren (löschen xs k)
+--     where k = kleinstes xs
+
+filter_ :: (Int -> Bool) -> [Int] -> [Int]
+filter_ f [] = []
+filter_ f (x:xs) 
+    | f x == True = x:filter_ f xs
+    | otherwise = filter_ f xs
+
+zipper :: (Int -> Int -> Int) -> [Int] -> [Int] -> [Int]
+zipper f [] [] = []
+zipper f (x:xs) (y:ys) = (f x y):zipper f xs ys  
+
+folder :: (Int -> Int -> Int) -> Int -> [Int] -> Int
+folder f y [] = y
+folder f n [x] = f n x
+folder f n (x:y:ys) = folder f n (ys ++ [f x y])
+
+insertion :: [Int] -> Int -> [Int]
+insertion [] n = [n]
+insertion (x:xs) n
+    | n > x = x:insertion xs n
+    | otherwise = [n] ++ (x:xs)
+
+insertionSort :: [Int] -> [Int]
+insertionSort [] = []
+insertionSort [x] = [x]
+insertionSort (x:xs) = insertion (insertionSort xs) x
+
+bubble :: [Int] -> [Int]
+bubble [] = []
+bubble [x] = [x]
+bubble (x:y:ys)
+    | x <= y = x:bubble (y:ys)
+    | otherwise = y:bubble (x:ys)
+
+bubbleSort :: [Int] -> [Int]
+bubbleSort [] = []
+bubbleSort (x:xs) = bubble (x:bubbleSort xs)
+
+merge :: [Int] -> [Int] -> [Int]
+merge [] [] = []
+merge [x] [y]
+    | x <= y = [x, y]
+    | otherwise = [y, x]
+merge (x:xs) [] = (x:xs)
+merge [] (y:ys) = (y:ys)
+merge (x:xs) (y:ys)
+    | x <= y = insertionSort (x:merge xs (y:ys))
+    | otherwise = insertionSort (y:merge (x:xs) ys)
